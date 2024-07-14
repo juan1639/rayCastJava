c67df87fec498f34a45a31c66988ad71cd2caf80
unknown <juaneguiabad1639@gmail.com>
2024-07-14 20:51:06 +0200

    class Settings cambiada a constantes static

--- src/main/java/juan1639/main/Ventana.java
+++ src/main/java/juan1639/main/Ventana.java
@@ -1,9 +1,39 @@
 package main.java.juan1639.main;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
+
+import main.java.juan1639.settings.Settings;
 
 public class Ventana extends JPanel {
 	
+	public static final int WIDTH = Settings.Dimensiones.TILE_X * Settings.Dimensiones.NRO_COLUMNAS; 
+	public static final int HEIGHT = Settings.Dimensiones.TILE_Y * Settings.Dimensiones.NRO_FILAS; 
+	
 	private static final long serialVersionUID = 6132970227030811138L;
+	
+	public Ventana() {
+		
+		inicializa();
+	}
+	
+	private void inicializa() {
+		
+		//addKeyListener(new Controles());
+
+		int[] rgb = Settings.Colores.GRIS_SUELO;
+		setBackground(new Color(rgb[0], rgb[1], rgb[2]));
+		setFocusable(true);
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+		comenzar();
+	}
+	
+	private void comenzar() {
+		
+		
+	}
 
 }
--- src/main/java/juan1639/settings/Settings.java
+++ src/main/java/juan1639/settings/Settings.java
@@ -1,5 +1,38 @@
 package main.java.juan1639.settings;
 
 public class Settings {
-
+	
+	public static final int FPS = 120;
+	
+	public static final Double PI = 3.14159265;
+	
+	public static final int DISTANCIA_ENTRE_RAYOS = 1;// 1=ideal, 2=para más FPS, etc...
+	public static final int FOV = 60;// 60º (Field Of View) del jugador
+	
+	public static final Boolean RENDER_CON_TEXTURA = false;
+	public static final int NRO_TEXTURAS_PARED = 14;
+	
+	// ==================================================
+	public class Dimensiones {
+		
+		public static final int TILE_X = 20;// px
+		public static final int TILE_Y = 20;// px
+		public static final int NRO_COLUMNAS = 40;
+		public static final int NRO_FILAS = 30;
+	}
+	
+	// ==================================================
+	public class Colores {
+		
+		public static final int[] BLANCO = {245, 245, 245};
+		public static final int[] GRIS_SUELO = {70, 75, 75};
+		public static final int[] PARED_OSCURO = {150, 150, 150};
+		public static final int[] PARED_CLARO = {128, 128, 128};
+		public static final int[] NEGRO = {5, 5, 5};
+		public static final int[] AMARILLO = {249, 216, 28};
+		public static final int[] VERDE = {83, 216, 61};
+		public static final int[] ROJO = {255, 55, 5};
+		public static final int[] AZUL_OSCURO = {22, 11, 78};
+		public static final int[] AZUL_CELESTE = {83, 183, 255};
+	}
 }
